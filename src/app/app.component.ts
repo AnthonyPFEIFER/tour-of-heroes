f202f22e326630069510559f7203c90ae3039048
AnthonyPFEIFER <anthhony.pfeifer@gmail.com>
2022-09-22 09:21:26 +0200

    CRUD Tour of Heroes working without backend yet

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { Hero } from 'src/app/interfaces/hero';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Tour of Kaamelott';
+
+}
